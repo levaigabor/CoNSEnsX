0d0dd8c1915197f3cb115b225987b0ea9fac7efa
Daniel Dudola <daniel.dudola@gmail.com>
2014-09-23 11:03:44 +0200

    added client side

---
+++ csx_libs/ouput.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import time
+
+date = time.strftime("%a %d. %b %X %Z %Y")
+
+def writeHeaderTXT(args, version):
+    txt = args.output_file_name + ".txt"
+    txt = open(txt, 'w')
+
+    txt.write("CoNSEnsX version " + version + " started on " + date + "\n")
+    txt.write("========================================================\n")
+    txt.write("Input files specified:" +
+                   "\n\tPDB file: "                 + args.PDB_file +
+                   "\n\tX-PLOR restraint file: "    + args.XPLOR_file +
+                   "\n\tBMRB file: "                + args.STR_file + "\n\n")
+
+
+def writeHeaderHTML(args, version):
+    html = args.output_file_name + ".html"
+    html  = open(html, 'w')
+
+    html.write( "<html><head><title>CoNSEnsX output</title></head>\n" +
+                "<body bgcolor=white><h2>CoNSEnsX version " + version +
+                "</h2>\n<small>started on " + date + " </small><br>\n")
+
+    html.write( "<br><b>Input files specified:</b>\n<ul>\n" +
+                "<li>PDB file: " + args.PDB_file + "</li>\n" +
+                "<li>X-PLOR restraint file: " + args.XPLOR_file + "</li>\n" +
+                "<li>BMRB file: " + args.STR_file + "</li>\n</ul>\n<table>\n")
