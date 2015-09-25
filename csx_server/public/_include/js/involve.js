b73e2dc54ec3e352a90496a4216f35dc5d7629ef
derPuntigamer <daniel.dudola@gmail.com>
2015-09-25 15:51:51 +0200

    add: involve controls by result list items

--- csx_server/public/_include/js/involve.js
+++ csx_server/public/_include/js/involve.js
@@ -3,8 +3,20 @@ $(document).ready(function() {
 		$(".involve-result").toggleClass ( "involve", 1000, "easeOutSine")
 	});
 	
-	$( ".show" ).click(function() {
+	$( ".inv-1" ).click(function() {
   		$(".group3").toggleClass( "involve", 1000, "easeOutSine" );
+	});
+
+	$( ".inv-2" ).click(function() {
+  		$(".group4").toggleClass( "involve", 1000, "easeOutSine" );
+	});
+
+	$( ".inv-3" ).click(function() {
+  		$(".group5").toggleClass( "involve", 1000, "easeOutSine" );
+	});
+
+	$( ".inv-4" ).click(function() {
+  		$(".group6").toggleClass( "involve", 1000, "easeOutSine" );
 	});
 
 });
