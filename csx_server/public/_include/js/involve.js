7a016565c0d2a73b6c46b573074c4c8bc58ca9d1
derPuntigamer <daniel.dudola@gmail.com>
2015-09-29 20:49:19 +0200

    add: result page selection function

--- csx_server/public/_include/js/involve.js
+++ csx_server/public/_include/js/involve.js
@@ -2,21 +2,4 @@ $(document).ready(function() {
 	$( "#selection").click(function () {
 		$(".involve-result").toggleClass ( "involve", 1000, "easeOutSine")
 	});
-	
-	$( ".inv-1" ).click(function() {
-  		$(".group3").toggleClass( "involve", 1000, "easeOutSine" );
-	});
-
-	$( ".inv-2" ).click(function() {
-  		$(".group4").toggleClass( "involve", 1000, "easeOutSine" );
-	});
-
-	$( ".inv-3" ).click(function() {
-  		$(".group5").toggleClass( "involve", 1000, "easeOutSine" );
-	});
-
-	$( ".inv-4" ).click(function() {
-  		$(".group6").toggleClass( "involve", 1000, "easeOutSine" );
-	});
-
 });
